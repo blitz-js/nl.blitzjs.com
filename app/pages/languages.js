b73e2e7d6fddc5d2d08fab923f6006c655e57f15
Dylan Kiss <dylankiss10@gmail.com>
2021-03-26 00:36:55 +0100

    Translate Languages

--- app/pages/languages.js
+++ app/pages/languages.js
@@ -34,9 +34,9 @@ const LanguagesPage = ({languages}) => {
       ></div>
       <main className="mx-auto max-w-7xl px-6 py-24 xl:py-36 text-black dark:text-dark-mode-text space-y-16 lg:space-y-20">
         <div className="space-y-6">
-          <h1 className="font-primary text-3xl lg:text4xl xl:text-5xl font-semibold">Languages</h1>
+          <h1 className="font-primary text-3xl lg:text4xl xl:text-5xl font-semibold">Talen</h1>
           <p className="font-secondary text-lg text-gray-600 dark:text-gray-300">
-            The Blitz documentation is currently being translated into the following languages:
+            De Blitz documentatie wordt momenteel vertaald in de volgende talen:
           </p>
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-x-12 gap-y-6">
@@ -49,21 +49,21 @@ const LanguagesPage = ({languages}) => {
                   href={`https://github.com/blitz-js/${lang.code}.blitzjs.com/issues/1`}
                   className="text-purple-light dark:text-purple-extralight font-medium dark:font-bold no-underline dark:underline hover:underline"
                 >
-                  Help Translate
+                  Help met vertalen
                 </a>
               </p>
             </div>
           ))}
         </div>
         <div className="font-secondary text-base text-gray-600 dark:text-gray-300">
-          Don't see your language?{" "}
+          Zie je je taal niet?{" "}
           <a
             href="https://github.com/blitz-js/blitzjs.com-translation#starting-a-new-translation"
             target="_blank"
             rel="noreferrer"
             className="text-purple-light dark:text-purple-extralight font-medium dark:font-bold no-underline dark:underline hover:underline"
           >
-            Read how to start a new translation
+            Lees hier hoe je een nieuwe vertaling start
           </a>
         </div>
       </main>
@@ -130,8 +130,8 @@ const getStaticProps = async () => {
 }
 
 LanguagesPage.meta = {
-  title: "Languages - Blitz.js",
-  description: `Blitz is a hyper-productive fullstack React framework that's built on Next.js and features a "Zero-API" data layer.`,
+  title: "Talen - Blitz.js",
+  description: `Blitz is een hyperproductief fullstack React-framework dat is gebouwd op Next.js en beschikt over een "Zero-API" data layer.`,
 }
 
 export default LanguagesPage
